be6d0579f8e08da8732a9ebfc8807cd61c805ce7
ShabazBadshah <badshah.shabaz.dev@gmail.com>
2020-10-07 21:31:40 -0400

    Updates

---
+++ media-query-sizes.js
@@ -0,0 +1,10 @@
+export const deviceMaxWidth = {
+  mobileS: '(max-width: 320px)',
+  mobileM: '(max-width: 375px)',
+  mobileL: '(max-width: 480px)',
+  tablet: '(max-width: 768px)',
+  laptop: '(max-width: 1024px)',
+  laptopL: '(max-width: 1440px)',
+  desktop: '(max-width: 2560px)',
+  desktopL: '(max-width: 2560px)',
+};
